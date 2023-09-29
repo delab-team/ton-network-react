a4eb56dbdcbc6ae0cc29f0d1f4e318e539ab76dd
sijuz <sijuz@yandex.ru>
2023-09-29 10:11:22 +0300

    s

--- src/index.tsx
+++
@@ -1,4 +0,0 @@
-import { NetworkProvider, Sender, SenderArguments, UIProvider, ProviderTonConnect, SenderTonConnect, UIProviderTonConnect } from './logic'
-
-export type { NetworkProvider, Sender, SenderArguments, UIProvider }
-export { ProviderTonConnect, SenderTonConnect, UIProviderTonConnect }
